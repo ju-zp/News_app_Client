08d22b92e94b955e3e2ed3ac2cb4a8700380f344
ju-zp <jhuntington@tutanota.com>
2019-02-05 14:50:44 +0000

    updated navigation file with basics

---
+++ screens/news/components/Navigation.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import { View, Text } from 'react-navigation'
+
+class Navigation extends Component {
+
+    render(){
+        return <View>
+            <Text>Navigation</Text>
+        </View>
+    }
+}
+
+export default Navigation
