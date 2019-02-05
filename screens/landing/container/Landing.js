65876c365a841b1a532d69f03d6c7d090a5566ce
ju-zp <jhuntington@tutanota.com>
2019-02-05 14:29:33 +0000

    added fade animation on anding text

--- screens/landing/container/Landing.js
+++ screens/landing/container/Landing.js
@@ -1,27 +1,43 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Animated, StyleSheet, Text, View } from 'react-native'
 
 const styles = StyleSheet.create({
     screen: {
-        backgroundColor: 'yellow',
+        backgroundColor: '#1f2833',
         flex: 1,
         justifyContent: 'center',
     },
     title: {
         left: '33%',
-        fontSize: 30
+        fontSize: 30,
+        color: '#66fcf1'
     }
 })
 
 class Landing extends Component {
+
+    state = {
+        fadeAni: new Animated.Value(0),
+        interval: null
+    }
+
+    componentDidMount(){
+        Animated.timing(
+            this.state.fadeAni,{
+                toValue: 1,
+                duration: 1000
+            }
+        ).start()
+    }
 
     static navigationOptions = {
         title: 'Landing',
         header: null
     }
     render(){
+        const { fadeAni } = this.state
         return <View style={styles.screen}>
-                <Text style={styles.title}>NewApp</Text>
+                <Animated.Text style={{...styles.title, opacity: fadeAni}}>NewsApp</Animated.Text>
             </View>
     }
 }
